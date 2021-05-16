6a5291c18fc290f968b8cad059847c3740aa76f8
David P <fake.david.123456@gmail.com>
2021-05-16 16:57:59 +1000

    combined comfigs into one for theurge.com

--- grabba/grabba/spiders/part2.py
+++ grabba/grabba/spiders/part2.py
@@ -25,19 +25,13 @@ class Part2Spider(CrawlSpider):
         super().__init__()
 
     def start_requests(self):
-        yield Request("https://theurge.com/women/search/?cat=clothing-dresses")
+        for url in self.config["start_pages"]:
+            yield Request(url)
 
     def parse_item(self, response):
         item = scrapy.Item()
         l = ItemLoader(item=item, response=response)
-        '''
-        l.add_css("title", "span._3mRKt::text", Join(), MapCompose(str.strip))
-        l.add_css("sale_price", "div.eP0wn._26-lJ._28iFq::text", Join(), MapCompose(str.strip), re='[,.0-9]+')
-        l.add_css("full_price", "span._2plVT._35rbh::text", re="[,.0-9]+")
-        l.add_css("description", "div._34YUR._1K7NF > span::text", MapCompose(str.strip))
-        l.add_css("brand", "h1._1psEi > a::text")
-        l.add_css("category", "li._1Hb_0:nth-child(4) > a > span::text")
-        '''
+        
         for a in self.config["attributes"]:
             item.fields[a["name"]] = scrapy.Field()
             
